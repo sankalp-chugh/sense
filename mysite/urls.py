b6f49635329271b71efbbef989c81b7f61762c49
sankalp-chugh <sankalp.chugh@gmail.com>
2015-10-01 03:04:22 +0530

    urls.py updated with settings import

--- mysite/urls.py
+++ mysite/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
 ]
 
 urlpatterns += patterns('',
- (r'^static/(?P.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+ (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
  )
