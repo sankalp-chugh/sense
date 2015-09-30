91a62733a97a3abcaa0d6f9ff2a6e083af42b8c8
sankalp-chugh <sankalp.chugh@gmail.com>
2015-10-01 00:06:10 +0530

    whitenoise simplified

--- mysite/settings.py
+++ mysite/settings.py
@@ -100,3 +100,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
