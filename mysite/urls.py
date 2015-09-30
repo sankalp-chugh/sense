71f6fdf4eed9f301e0d0563cb838c48107f73cbc
sankalp-chugh <sankalp.chugh@gmail.com>
2015-10-01 03:01:03 +0530

    urls.py updated with settings import

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import include, url, patterns
 from django.contrib import admin
+from django.conf import settings
 
 urlpatterns = [
     url(r'^polls/', include('polls.urls')),
